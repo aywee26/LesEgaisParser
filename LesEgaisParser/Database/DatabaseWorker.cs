c0f98daee836a84f49e761d022507f17f0786a71
aywee26 <aywee26@gmail.com>
2023-03-01 15:32:18 +0300

    Implement basic Db class.

---
+++ LesEgaisParser/Database/DatabaseWorker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LesEgaisParser.Database
+{
+    public class DatabaseWorker
+    {
+        private readonly string _connectionString;
+
+        public DatabaseWorker(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void OutputWoodDealsToConsole()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string sqlExpression = "SELECT * FROM WoodDeals";
+                var command = new SqlCommand(sqlExpression, connection);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            string dealNumber = reader.GetString(0);
+                            string sellerName = reader.GetString(1);
+                            string sellerInn = reader.GetString(2);
+                            string buyerName = reader.GetString(3);
+                            string buyerInn = reader.GetString(4);
+                            DateTime dealDate = reader.GetDateTime(5);
+                            decimal woodVolumeBuyer = reader.GetDecimal(6);
+                            decimal woodVolumeSeller = reader.GetDecimal(7);
+
+                            Console.WriteLine($"{dealNumber}\t{sellerName}\t{sellerInn}\t{buyerName}\t{buyerInn}\t{dealDate}\t{woodVolumeBuyer}\t{woodVolumeSeller}");
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
--- LesEgaisParser/Program.cs
+++ LesEgaisParser/Program.cs
@@ -1,5 +1,4 @@
-﻿using LesEgaisParser.Mapping;
-using System;
+﻿using LesEgaisParser.Database;
 using System.Configuration;
 
 namespace LesEgaisParser
@@ -8,22 +7,27 @@ namespace LesEgaisParser
     {
         static void Main(string[] args)
         {
-            var numbersOfDeals = int.Parse(ConfigurationManager.AppSettings["NumberOfDealsPerRequest"]);
-            var requestDelay = int.Parse(ConfigurationManager.AppSettings["RequestDelay"]);
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            var dbWorker = new DatabaseWorker(connectionString);
+            dbWorker.OutputWoodDealsToConsole();
 
-            var httpClient = new HttpClientDemo(numbersOfDeals, requestDelay);
 
-            Console.WriteLine($"Total number of deals: {httpClient.RequestTotalNumberOfDeals()}");
+            //var numbersOfDeals = int.Parse(ConfigurationManager.AppSettings["NumberOfDealsPerRequest"]);
+            //var requestDelay = int.Parse(ConfigurationManager.AppSettings["RequestDelay"]);
 
-            var dealsDto = httpClient.RequestSpecialReportWoodDeal(page: 1);
-            var mapper = new WoodDealMapper();
-            var deals = mapper.GetWoodDeals(dealsDto);
-            Console.WriteLine("Example of deserialized wood deal:");
-            Console.WriteLine("Deal number: " + deals[0].DealNumber);
-            Console.WriteLine("Seller name: " + deals[0].SellerName);
-            Console.WriteLine("Seller INN: " + deals[0].SellerInn);
-            Console.WriteLine("How much was sold: " + deals[0].WoodVolumeSeller);
-            Console.ReadLine();
+            //var httpClient = new HttpClientDemo(numbersOfDeals, requestDelay);
+
+            //Console.WriteLine($"Total number of deals: {httpClient.RequestTotalNumberOfDeals()}");
+
+            //var dealsDto = httpClient.RequestSpecialReportWoodDeal(page: 1);
+            //var mapper = new WoodDealMapper();
+            //var deals = mapper.GetWoodDeals(dealsDto);
+            //Console.WriteLine("Example of deserialized wood deal:");
+            //Console.WriteLine("Deal number: " + deals[0].DealNumber);
+            //Console.WriteLine("Seller name: " + deals[0].SellerName);
+            //Console.WriteLine("Seller INN: " + deals[0].SellerInn);
+            //Console.WriteLine("How much was sold: " + deals[0].WoodVolumeSeller);
+            //Console.ReadLine();
         }
     }
 }
