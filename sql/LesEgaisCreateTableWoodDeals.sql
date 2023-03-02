106c199e19dc1a72228e73c279db6a5855ab8a2b
aywee26 <aywee26@gmail.com>
2023-03-02 23:10:24 +0300

    Fix Sql query.

--- sql/LesEgaisCreateTableWoodDeals.sql
+++ sql/LesEgaisCreateTableWoodDeals.sql
@@ -2,13 +2,14 @@ USE LesEgais;
 GO
 
 CREATE TABLE WoodDeals (
-	DealNumber nvarchar(28) PRIMARY KEY,
-	SellerName nvarchar(max),
+	DealNumber nvarchar(28) NOT NULL,
+	SellerName nvarchar(max) NOT NULL,
 	SellerInn nvarchar(12),
-	BuyerName nvarchar(max),
+	BuyerName nvarchar(max) NOT NULL,
 	BuyerInn nvarchar(12),
-	DealDate date,
-	WoodVolumeBuyer decimal(18, 2),
-	WoodVolumeSeller decimal(18, 2)
+	DealDate date NOT NULL,
+	WoodVolumeBuyer decimal(18, 2) NOT NULL,
+	WoodVolumeSeller decimal(18, 2) NOT NULL,
+	PRIMARY KEY CLUSTERED (DealNumber)
 );
 GO
