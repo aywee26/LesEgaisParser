33461e33c9726877b1ee22f7f9e2db16a0be4f2c
WOTTAKWOT <aywee26@gmail.com>
2023-03-02 16:46:06 +0300

    Refactor parser code.

---
+++ LesEgaisParser/Parser/Parser.cs
@@ -0,0 +1,76 @@
+﻿using LesEgaisParser.Database;
+using LesEgaisParser.Mapping;
+using System;
+using System.Threading;
+
+namespace LesEgaisParser.Parser
+{
+    public class ParserClass
+    {
+        private readonly string _connectionString;
+        private readonly int _numberOfDealsPerPage;
+        private readonly TimeSpan _requestDelay;
+        private readonly TimeSpan _delayBetweenParsing;
+        private readonly DatabaseWorker _dbWorker;
+        private readonly HttpClientAdapter _httpClient;
+        private readonly WoodDealMapper _mapper;
+
+        public ParserClass(string connectionString, int numberOfDealsPerPage, TimeSpan requestDelay, TimeSpan delayBetweenParsing)
+        {
+            _connectionString = connectionString;
+            _numberOfDealsPerPage = numberOfDealsPerPage;
+            _requestDelay = requestDelay;
+            _delayBetweenParsing = delayBetweenParsing;
+
+            _dbWorker = new DatabaseWorker(_connectionString);
+            _httpClient = new HttpClientAdapter(_numberOfDealsPerPage, _requestDelay);
+            _mapper = new WoodDealMapper();
+        }
+
+        public void LaunchParsing()
+        {
+            do
+            {
+                PerformParsing();
+                Thread.Sleep(_delayBetweenParsing);
+            }
+            while (true);
+        }
+
+        private void PerformParsing()
+        {
+            Console.WriteLine("Testing connection to DB...");
+            _dbWorker.TestConnectionToDb();
+
+            Console.WriteLine("Requesting number of deals to process...");
+            var dealsTotal = _httpClient.RequestTotalNumberOfDeals();
+            var pages = (int)Math.Ceiling((decimal)dealsTotal / _numberOfDealsPerPage);
+            Console.WriteLine("Deals to process: " + dealsTotal);
+            Console.WriteLine("Pages to process: " + pages);
+            Console.WriteLine();
+
+            for (int i = 0; i < pages; i++)
+            {
+                Console.WriteLine($"Processing page {i}...");
+                var pageContent = _httpClient.RequestSpecialReportWoodDeal(i);
+
+                if (pageContent == null)
+                {
+                    Console.WriteLine("Nothing to deserialize!");
+                    Console.WriteLine($"Page {i} skipped!");
+                    continue;
+                }
+
+                Console.WriteLine("Deserializing data...");
+                var deserializedContent = _mapper.GetWoodDeals(pageContent);
+
+                Console.WriteLine("Attempting to upsert data...");
+                _dbWorker.UpsertWoodDeals(deserializedContent);
+
+                Console.WriteLine("Success!\n");
+            }
+        }
+
+
+    }
+}
--- LesEgaisParser/Program.cs
+++ LesEgaisParser/Program.cs
@@ -1,9 +1,6 @@
-﻿using LesEgaisParser.Database;
-using LesEgaisParser.Mapping;
+﻿using LesEgaisParser.Parser;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Threading;
 
 namespace LesEgaisParser
 {
@@ -12,54 +9,14 @@ namespace LesEgaisParser
         public static void Main(string[] args)
         {
             var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            var numbersOfDeals = int.Parse(ConfigurationManager.AppSettings["NumberOfDealsPerRequest"]);
+            var numberOfDeals = int.Parse(ConfigurationManager.AppSettings["NumberOfDealsPerRequest"]);
             var requestDelaySeconds = int.Parse(ConfigurationManager.AppSettings["DelayBetweenRequestsSeconds"]);
             var requestDelay = TimeSpan.FromSeconds(requestDelaySeconds);
+            var delayBetweenParsingMinutes = int.Parse(ConfigurationManager.AppSettings["DelayBetweenParsingMinutes"]);
+            var delay = TimeSpan.FromMinutes(delayBetweenParsingMinutes);
 
-            Console.WriteLine("Testing connection to DB...");
-            var dbWorker = new DatabaseWorker(connectionString);
-            dbWorker.TestConnectionToDb();
-
-            Console.WriteLine("Starting HttpClient...");
-            var httpClient = new HttpClientAdapter(numbersOfDeals, requestDelay);
-
-            Console.WriteLine("Requesting number of deals to process...");
-            var dealsTotal = httpClient.RequestTotalNumberOfDeals();
-            var pages = (int)Math.Ceiling((decimal)dealsTotal / numbersOfDeals);
-            Console.WriteLine("Deals to process: " + dealsTotal);
-            Console.WriteLine("Pages to process: " + pages);
-            Console.WriteLine();
-
-            var mapper = new WoodDealMapper();
-
-            var skippedPages = new List<int>();
-
-            for (int i = 0; i < pages; i++)
-            {
-                Thread.Sleep(requestDelay);
-                Console.WriteLine($"Processing page {i}...");
-                var pageContent = httpClient.RequestSpecialReportWoodDeal(i);
-
-                if (pageContent == null)
-                {
-                    Console.WriteLine("Nothing to deserialize!");
-                    Console.WriteLine("Page skipped!");
-                    skippedPages.Add(i);
-                    continue;
-                }
-
-                Console.WriteLine("Deserializing data...");
-                var deserializedContent = mapper.GetWoodDeals(pageContent);
-                
-                Console.WriteLine("Attempting to upsert data...");
-                dbWorker.UpsertWoodDeals(deserializedContent);
-
-                Console.WriteLine("Success!");
-            }
-
-            Console.WriteLine("Parsing is done!");
-            Console.WriteLine("Skipped pages: " + string.Join(", ", skippedPages));
-            Console.ReadLine();
+            var parser = new ParserClass(connectionString, numberOfDeals, requestDelay, delay);
+            parser.LaunchParsing();
         }
     }
 }
